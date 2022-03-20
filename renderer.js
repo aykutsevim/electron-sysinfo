f3d6d41c941ebaaa4a9bd74ab61eba70a3b607cc
Aykut Sevim <aykut.sevim@hotmail.com>
2022-03-21 00:47:31 +0300

    added texture

--- renderer.js
+++ renderer.js
@@ -5,24 +5,37 @@
 // selectively enable features needed in the rendering
 // process.
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
+camera.position.z = 400;
 
-const renderer = new THREE.WebGLRenderer();
+const texture = new THREE.TextureLoader().load( 'textures/crate.gif' );
+
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );		
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const geometry = new THREE.BoxGeometry( 200, 200, 200 );
+const material = new THREE.MeshBasicMaterial( { map: texture } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+window.addEventListener( 'resize', onWindowResize );
 
 function animate() {
   requestAnimationFrame( animate );
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  
+  cube.rotation.x += 0.001;
+  cube.rotation.y += 0.001;
+  cube.scale.z += 0.001;
   renderer.render( scene, camera );
 }
 animate();
+
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
